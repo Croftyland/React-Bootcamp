0e8ca519c5bd4416bdd8e941acc14f5e2efe4195
Christina Landvytovych <Crofty.land@gmail.com>
2018-11-21 14:00:42 +0200

    optimization

--- template/src/components/Filters/Filters.jsx
+++ template/src/components/Filters/Filters.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import SortBy from './SortBy'
 
 export default class Filters extends React.Component {
     render() {
@@ -10,25 +11,14 @@ export default class Filters extends React.Component {
         } = this.props;
         return (
             <form className="mb-3">
-                <div className="form-group">
-                    <label htmlFor="sort_by">Сортировать по:</label>
-                    <select className="form-control"
-                            id="sort_by"
-                            name="sort_by"
-                            value={sort_by}
-                            onChange={onChangeFilters}
-                    >
-                        <option value="popularity.desc">Популярные по убыванию</option>
-                        <option value="popularity.asc">Популярные по возростанию</option>
-                        <option value="vote_average.desc">Рейтинг по убыванию</option>
-                        <option value="vote_average.asc">Рейтинг по возростанию</option>
-                    </select>
-                </div>
+                <SortBy sort_by={sort_by}
+                        onChangeFilters={onChangeFilters}
+                />
                 <div className="btn-group">
                     <button type="button"
                             className="btn btn-light"
                             disabled={page === 1}
-                            onClick={onChangePage.bind(null,page - 1)}
+                            onClick={onChangePage.bind(null, page - 1)}
                     >Назад
                     </button>
 
